b66856f42b5ebb4be4886b9090d3770e395e4585
Raghu Betina <raghu@firstdraft.com>
2016-12-03 02:23:46 +0000

    Whitelist columns in Day for ActiveAdmin

--- app/admin/day.rb
+++ app/admin/day.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Day do
 
+ permit_params :date
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
